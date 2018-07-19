91f40c8dc7c680d523964dc8087b7ceb77a69746
mattgif <matthew.gifford@leverege.com>
2018-07-19 16:12:52 -0400

    Separated project ID/project name

--- setup.sh
+++ setup.sh
@@ -7,13 +7,13 @@ echo
 
 # get device name, project name and device public key from user
 echo
-read -p 'Please enter your project ID (note: this may be different than the project name): ' GOOGLE_CLOUD_PROJECT
+read -p 'Please enter your project ID (note: this may be different than the project name): ' GCP_ID
 echo
 read -p 'Please enter device ID: ' DEVICE_ID
 echo
 
 # set the project and tell firebase to use it firebase
-gcloud config set project $GOOGLE_CLOUD_PROJECT
+gcloud config set project $GCP_ID
 firebase use $GOOGLE_CLOUD_PROJECT
 
 # enable cloud functions, IoT core, and pub sub
@@ -30,13 +30,13 @@ gcloud iot devices create "d$DEVICE_ID" --region=$CLOUD_REGION --registry=AVR-IO
 
 #install npm dependencies
 # TODO: check relative paths after clone in shell
-echo Installing Cloud Function dependencies (this may take a few minutes)...
+echo Installing Cloud Function dependencies \(this may take a few minutes\)...
 npm install --prefix ./functions/
-echo Installing UI dependencies (this may take a few minutes)...
+echo Installing UI dependencies \(this may take a few minutes\)...
 npm install --prefix ./ui/
 
 # build UI
-echo Creating a production build of the UI (this may take a few minute)...
+echo Creating a production build of the UI \(this may take a few minute\)...
 npm run build --prefix ./ui
 
 # retrieve UI config vars 
@@ -47,4 +47,4 @@ firebase deploy --only functions:recordMessage
 firebase deploy --only database
 firebase deploy --only hosting
 
-echo Setup complete! Don't forget to add your device's public key in the registry: https://console.cloud.google.com/iot/registries
+echo Setup complete! Don\'t forget to add your device\'s public key in the registry: https://console.cloud.google.com/iot/registries
