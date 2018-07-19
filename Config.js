cf05d04fa04996d5aa46a9d9de6465435b51e045
mattgif <matthew.gifford@leverege.com>
2018-07-19 18:12:24 -0400

    Fixed bug due to comment in JSON file

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
+// FIREBASE_CONFIG is automatically configured by setup.sh with project config json
 const Config = {
   FIREBASE_BASE_URL: '/avr-iot/data/',
-  // FIREBASE_CONFIG is automatically configured by setup.sh with project config json
   FIREBASE_CONFIG: '<fbconfig>'
 }
 
