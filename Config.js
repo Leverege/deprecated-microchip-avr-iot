beb6f40ceaa3b115b768eb676bc9ee80929bd8d9
mattgif <matthew.gifford@leverege.com>
2018-07-19 17:52:00 -0400

    Changed Config export

--- Config.js
+++ Config.js
@@ -1,5 +1,7 @@
-export const Config = {
+const Config = {
   FIREBASE_BASE_URL: '/avr-iot/data/',
   // FIREBASE_CONFIG is automatically configured by setup.sh with project config json
   FIREBASE_CONFIG: '<fbconfig>'
 }
+
+exports.Config = Config;
