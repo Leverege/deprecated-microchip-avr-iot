8bee091dd84ad318194f10a216f2ae882086078f
Matthew Gifford <mattgif@gmail.com>
2018-07-27 10:42:28 -0400

    stripping whitespace from raw user input

--- setup.sh
+++ setup.sh
@@ -16,6 +16,9 @@ echo
 read -p 'Please enter device ID: ' RAW_DEVICE_ID
 echo
 
+# trim whitespace from device id input
+DEVICE_ID = "$(echo -e "${RAW_DEVICE_ID}" | tr -d '[:space:]')"
+
 # set the project and tell firebase to use it firebase
 gcloud config set project $GOOGLE_CLOUD_PROJECT
 firebase use $GOOGLE_CLOUD_PROJECT
